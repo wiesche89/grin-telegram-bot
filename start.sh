0f54680d904cba87025f19be96ed76eb197367b1
Jörg Wieschebrock <Joerg.Wieschebrock@krone.de>
2025-07-03 20:42:45 +0200

    add start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Fix permissions for Tor onion service directories
+find /root/.grin/main/tor/listener/onion_service_addresses -type d -exec chmod 700 {} \;
+
+# Read password from file and pipe it to grin-wallet listen, running in background
+cat /opt/grin-telegram-bot/wallet/password.txt | ./grin-wallet listen &
+
+# Start the grin-telegram-bot
+./grin-telegram-bot
