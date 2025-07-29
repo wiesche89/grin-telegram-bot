c6f2ad1f5401d63f508457f6490ae7191b32cda4
Joerg <wiesche89@googlemail.com>
2025-07-29 10:56:25 +0200

    l

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ ln -sf "$DATA_DIR/.grin" /root/.grin
 find /root/.grin/main/tor/listener/onion_service_addresses -type d -exec chmod 700 {} \;
 
 # Read password from file and pipe it to grin-wallet listen, running in background
-cat /opt/grin-telegram-bot/.wallet/password.txt | ./grin-wallet listen &
+cat /opt/grin-telegram-bot/data/.wallet/password.txt | ./grin-wallet listen &
 
 # Wait 30 seconds before starting the telegram bot
 sleep 30
